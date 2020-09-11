60277d5ce393a2b2388880dd46b986cefaee1c71
npaudyal <zlatan.nis111@gmail.com>
2020-09-11 14:58:53 -0500

    Service layer architecture added

--- src/com/paudyal/springdemo/controller/CustomerController.java
+++ src/com/paudyal/springdemo/controller/CustomerController.java
@@ -5,23 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.paudyal.springdemo.dao.CustomerDAO;
 import com.paudyal.springdemo.entity.Customer;
+import com.paudyal.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	//need to inject the customer service
+	
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 			
 		//get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		
 		//add customers to the model
